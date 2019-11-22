719cfa6f93e5c264680e5dc51ae4108bbdbe5205
lizw <lzw1000000@163.com>
2019-11-22 18:17:05 +0800

    ...

--- src/main/java/com/jzt/sonar/java/checks/named/NamedCheckConstant.java
+++ src/main/java/com/jzt/sonar/java/checks/named/NamedCheckConstant.java
@@ -16,3 +16,29 @@ public interface NamedCheckConstant {
 //-----------------------------------------------------------------------------------------------------------------------------------------
 //【推荐】各种命名必须表意准确、尽可能短，除2代替‘to’，4代替‘for’外，不建议在命名中使用数字
 //【推荐】通过方法名称要能识别方法的作用
+
+
+//
+//https://github.com/SonarSource/sonar-java/blob/master/docs/CUSTOM_RULES_101.md
+//
+//
+//  info 提示
+//  minor 次要
+//  major 只要
+//  critical 严重
+//  blocker 阻断
+//
+//  https://github.com/wychenzhou/sonarqube_java_custom_rule
+
+
+
+
+
+
+
+
+
+
+
+
+
